0860b65bd9ef1757daad57f21e0bb7caf6c6072c
C315346 <valentin.hasler@axa.ch>
2023-02-03 15:39:43 +0100

    fix IDE Errors

--- tests/fixtures/react-invalid.jsx
+++ tests/fixtures/react-invalid.jsx
@@ -1,11 +1,11 @@
 import { useState, FC } from 'react'
 
-export const Component: FC = ({children}) => {
+export const Component = ({children}) => {
     if (!children) {
         return null
     }
 
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState<any>(0)
     return (
         <h1 className="foo" onClick={() => setCount((v) => v + 1)}>
             {count}<br />
