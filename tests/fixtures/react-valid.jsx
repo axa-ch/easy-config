c6c051781ddfcff3ace1f11906d4fcad6f880cc6
C315346 <valentin.hasler@axa.ch>
2023-02-03 14:47:05 +0100

    remove todo

--- tests/fixtures/react-valid.jsx
+++ tests/fixtures/react-valid.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 
 export const Component = ({ children }) => {
-    const [count, setCount] = useState(0);
-    return (
-        <h1
-            className="foo"
-            onClick={() => setCount((v) => v + 1)}
-        >
-            {count}
-            <br />
-            {children}
-        </h1>
-    );
-}
+  const [count, setCount] = useState(0);
+  return (
+    <h1
+      className="foo"
+      onClick={() => setCount((v) => v + 1)}
+    >
+      {count}
+      <br />
+      {children}
+    </h1>
+  );
+};
