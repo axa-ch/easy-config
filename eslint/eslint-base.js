d166436fae0e649041ef876a4a6ca3156ddcf36c
C315346 <valentin.hasler@axa.ch>
2023-02-06 09:54:33 +0100

    remove fp rules

--- eslint/eslint-base.js
+++ eslint/eslint-base.js
@@ -11,12 +11,5 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 'off',
     'no-param-reassign': ['error', { props: false }],
-    'fp/no-class': 'off',
-    'fp/no-delete': 'off',
-    'fp/no-get-set': 'off',
-    'fp/no-let': 'off',
-    'fp/no-loops': 'off',
-    'fp/no-mutating-assign': 'off',
-    'fp/no-proxy': 'off',
   },
 };
