b71d6c0eddc026ee555fa78520a03df4d38e42ef
Guarini Gianluca Daniele <C428744@ch.doleni.net>
2023-02-07 08:30:06 +0100

    chore: Add small tweeks to the source code
    - Format the readme file using prettier
    - Use the latest parser option for the eslint configuration
    - Avoid redundant eslint options
    - Convert all the typescript strings to lower case
    - Update the project dependencies

--- eslint/eslint-typescript.js
+++ eslint/eslint-typescript.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended'],
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
