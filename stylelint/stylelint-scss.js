2f391abbc4df3c5e74ea65130e00d06c067f3649
Gianluca Guarini <gianluca.guarini@gmail.com>
2023-11-01 10:44:27 +0100

    fix: Typo

--- stylelint/stylelint-scss.js
+++ stylelint/stylelint-scss.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     'selector-class-pattern': null,
     'no-duplicate-selectors': null,
-    // this rule is deprecated and generate warnings in newer project setup
+    // this rule is deprecated and generates warnings in newer project setup
     'scss/at-import-no-partial-leading-underscore': null,
   },
 };
