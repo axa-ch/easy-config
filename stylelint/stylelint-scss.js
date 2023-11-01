34ee34322109f615ebb3a043f080e5364360487a
Gianluca Guarini <gianluca.guarini@gmail.com>
2023-11-01 10:43:33 +0100

    chore: Update deps and fix deprecated warnings

--- stylelint/stylelint-scss.js
+++ stylelint/stylelint-scss.js
@@ -3,5 +3,7 @@ module.exports = {
   rules: {
     'selector-class-pattern': null,
     'no-duplicate-selectors': null,
+    // this rule is deprecated and generate warnings in newer project setup
+    'scss/at-import-no-partial-leading-underscore': null,
   },
 };
