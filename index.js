f2b4a224e6fb4ab6a08f409937c9e6a0936bd228
corsin-ragettli <corsin.ragettli@axa.ch>
2024-05-23 13:30:34 +0200

    fix ci

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import prettierBase from "./prettier/base.js";
-import stylelintBase from "./stylelint/stylelint-base.js";
-import stylelintOrder from "./stylelint/stylelint-order.js";
-import stylelintScss from "./stylelint/stylelint-scss.js";
-import tsConfigBase from "./ts-config/base.json" with { type: "json" };
+import prettierBase from './prettier/base.js';
+import stylelintBase from './stylelint/stylelint-base.js';
+import stylelintOrder from './stylelint/stylelint-order.js';
+import stylelintScss from './stylelint/stylelint-scss.js';
+import tsConfigBase from './ts-config/base.json' with { type: 'json' };
 
 // Stylelint rules
 export const stylelint = {
