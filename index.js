a5e38f2ff2402c9ad2991de7c563e9cc8e5d8aef
corsin-ragettli <corsin.ragettli@axa.ch>
2024-05-30 15:10:15 +0200

    fix: remove tsSettings from index.js

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ import prettierBase from './prettier/base.js';
 import stylelintBase from './stylelint/stylelint-base.js';
 import stylelintOrder from './stylelint/stylelint-order.js';
 import stylelintScss from './stylelint/stylelint-scss.js';
-import tsConfigBase from './ts-config/base.json' with { type: 'json' };
 
 // Stylelint rules
 export const stylelint = {
   base: stylelintBase,
   scss: stylelintScss,
   order: stylelintOrder,
-};
-
-export const tsConfig = {
-  base: tsConfigBase,
 };
 
 export const prettier = {
